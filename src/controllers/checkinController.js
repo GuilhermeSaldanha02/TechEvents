9381901942bc3c25cc44b87ce3d292cdb5c2d543
guilhermesaldanha02 <guilhermesaldanha007@gmail.com>
2025-12-06 23:12:25 -0300

    feat/checkin:Implementação inicial do módulo de Check-in

--- src/controllers/checkinController.js
+++ src/controllers/checkinController.js
@@ -1,8 +1,15 @@
-realizarCheckin(idParticipante) {
-    const participante = this.participanteController.participantes
-        .find(p => p.id === idParticipante);
-    
-    // !!! BUG PROPOSITAL: Quebra se o ID não for achado (participante será 'undefined')
-    
-    return `Bem-vindo ao evento, ${participante.nome}! Seu crachá é: ${participante.tipoIngresso}`;
+class CheckinController {
+    constructor(participanteController) {
+        this.participanteController = participanteController;
+    }
+
+    realizarCheckin(idParticipante) {
+        const participante = this.participanteController.participantes
+            .find(p => p.id === idParticipante);
+        
+        // !!! BUG PROPOSITAL: Quebra se o ID não for achado
+        
+        return `Bem-vindo ao evento, ${participante.nome}! Seu crachá é: ${participante.tipoIngresso}`;
+    }
 }
+module.exports = CheckinController;
