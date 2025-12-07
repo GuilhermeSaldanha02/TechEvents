caaf84e203053e3d6fb7892172cd580280132564
guilhermesaldanha02 <guilhermesaldanha007@gmail.com>
2025-12-06 23:15:56 -0300

    feat/checkin Correção-de crash-ao-buscar-participante-inexistente

--- src/controllers/checkinController.js
+++ src/controllers/checkinController.js
@@ -7,7 +7,10 @@ class CheckinController {
         const participante = this.participanteController.participantes
             .find(p => p.id === idParticipante);
         
-        // !!! BUG PROPOSITAL: Quebra se o ID não for achado
+        // CORREÇÃO APLICADA: Validação se o participante existe
+        if (!participante) {
+            return "Erro: Participante não encontrado na lista.";
+        }
         
         return `Bem-vindo ao evento, ${participante.nome}! Seu crachá é: ${participante.tipoIngresso}`;
     }
